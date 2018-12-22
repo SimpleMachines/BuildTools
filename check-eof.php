ee90894bb747bc17acc071327fb0c917d2a25bf3
Jon Stovell <jonstovell@gmail.com>
2018-12-22 02:42:02 -0700

    Ignores random_compat library in check-eof.php
    Signed-off-by: Jon Stovell <jonstovell@gmail.com>

--- check-eof.php
+++ check-eof.php
@@ -28,6 +28,9 @@ $ignoreFiles = array(
 	// Minify Stuff.
 	'\./Sources/minify/[A-Za-z0-9/-]+\.php',
 
+	// random_compat().
+	'\./Sources/random_compat/\w+\.php',
+
 	// ReCaptcha Stuff.
 	'\./Sources/ReCaptcha/[A-Za-z0-9]+\.php',
 	'\./Sources/ReCaptcha/RequestMethod/[A-Za-z0-9]+\.php',
