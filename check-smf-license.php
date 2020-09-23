90d88dd448a68b6655bf9d078864e92d2624afa6
jdarwood007 <unmonitored+github@sleepycode.com>
2020-09-22 18:12:04 -0700

    Update versions and add generateDetailedVersion.php to the ignore list.

--- check-smf-license.php
+++ check-smf-license.php
@@ -4,11 +4,11 @@
  * Simple Machines Forum (SMF)
  *
  * @package SMF
- * @author Simple Machines http://www.simplemachines.org
+ * @author Simple Machines https://www.simplemachines.org
  * @copyright 2020 Simple Machines and individual contributors
  * @license https://www.simplemachines.org/about/smf/license.php BSD
  *
- * @version 2.1 RC1
+ * @version 2.1 RC3
  */
 
 // Stuff we will ignore.
@@ -59,6 +59,7 @@ $ignoreFiles = array(
 
 $ignoreFilesVersion = array(
 	'/buildTools/check-[A-Za-z0-9-_]+\.php',
+	'/buildTools/generateDetailedVersion\.php',
 );
 
 // No file? Thats bad.
