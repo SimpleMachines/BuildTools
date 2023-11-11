b15ecbcf4ff878362a8647759f08a7574a262176
jdarwood007 <unmonitored+github@sleepycode.com>
2023-11-10 17:07:59 -0800

    Update to support new locale and sub-directory languages

--- check-smf-languages.php
+++ check-smf-languages.php
@@ -12,6 +12,7 @@
  */
 
 // Stuff we will ignore.
+global $ignoreFiles, $currentVersion;
 $ignoreFiles = array(
 	'index\.php',
 	'\.(?!php)[^.]*$',
@@ -34,40 +35,14 @@ try
 			throw new Exception('Error: Could not locate SMF_LANG_VERSION');
 		$currentVersion = $versionResults[1];
 
-		$language_dir = './Themes/default/languages';
-		if (file_exists('./Languages'))
-			$language_dir = './Languages';
-
-		foreach (new DirectoryIterator($language_dir) as $fileInfo)
+		if (!file_exists('./Languages'))
+			checkLanguageDirectory('./Themes/default/languages', 'english');
+		else
 		{
-			if ($fileInfo->getExtension() == 'php')
-			{
-				foreach ($ignoreFiles as $if)
-					if (preg_match('~' . $if . '~i', $fileInfo->getFilename()))
-						continue 2;
-
-				if (($file = fopen($fileInfo->getPathname(), 'r')) !== false)
-				{
-					$contents = fread($file, 500);
-
-					// Just see if the basic match is there.
-					$match = '// Version: ' . $currentVersion;
-					if (!preg_match('~' . $match . '~i', $contents))
-						throw new Exception('Error: The version is missing or incorrect in ' . $fileInfo->getFilename());
-
-					// Get the file prefix.
-					preg_match('~([A-Za-z]+)\.english\.php~i', $fileInfo->getFilename(), $fileMatch);
-					if (empty($fileMatch))
-						throw new Exception('Error: Could not locate the file name in ' . $fileInfo->getFilename());
-
-					// Now match that prefix in a more strict mode.
-					$match = '// Version: ' . $currentVersion . '; ' . $fileMatch[1];
-					if (!preg_match('~' . $match . '~i', $contents))
-						throw new Exception('Error: The version with file name is missing or incorrect in ' . $fileInfo->getFilename());
-				}
-				else
-					throw new Exception('Unable to open file ' . $fileInfo->getFilename());
-			}
+			checkLanguageDirectory('./Languages/en-us', 'en-us');
+//			foreach (new DirectoryIterator('./Languages') as $dirInfo)
+//				if(!in_array($dirInfo->getFileName(), ['.', '..']) && is_dir($dirInfo->getPathname()))
+//					checkLanguageDirectory($dirInfo->getPathname());
 		}
 	}
 	else
@@ -77,4 +52,41 @@ catch (Exception $e)
 {
 	fwrite(STDERR, $e->getMessage());
 	exit(1);
+}
+
+function checkLanguageDirectory($language_dir, $lang)
+{
+	global $ignoreFiles, $currentVersion;
+
+	foreach (new DirectoryIterator($language_dir) as $fileInfo)
+	{
+		if ($fileInfo->getExtension() == 'php')
+		{
+			foreach ($ignoreFiles as $if)
+				if (preg_match('~' . $if . '~i', $fileInfo->getFilename()))
+					continue 2;
+
+			if (($file = fopen($fileInfo->getPathname(), 'r')) !== false)
+			{
+				$contents = fread($file, 500);
+
+				// Just see if the basic match is there.
+				$match = '// Version: ' . $currentVersion;
+				if (!preg_match('~' . $match . '~i', $contents))
+					throw new Exception('Error: The version is missing or incorrect in ' . $fileInfo->getPathname());
+
+				// Get the file prefix.
+				preg_match('~([A-Za-z]+)\.' . $lang . '\.php~i', $fileInfo->getFilename(), $fileMatch);
+				if (empty($fileMatch))
+					throw new Exception('Error: Could not locate the file name in ' . $fileInfo->getPathname());
+
+				// Now match that prefix in a more strict mode.
+				$match = '// Version: ' . $currentVersion . '; ' . $fileMatch[1];
+				if (!preg_match('~' . $match . '~i', $contents))
+					throw new Exception('Error: The version with file name is missing or incorrect in ' . $fileInfo->getPathname());
+			}
+			else
+				throw new Exception('Unable to open file ' . $fileInfo->getPathname());
+		}
+	}
 }
