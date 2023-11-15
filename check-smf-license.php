65bcc7a39a237fa4048c0b6a9212cdce45cc9db0
jdarwood007 <unmonitored+github@sleepycode.com>
2023-11-14 18:35:25 -0800

    Add a separate check for our index checks

--- check-smf-license.php
+++ check-smf-license.php
@@ -24,37 +24,9 @@ $ignoreFiles = array(
 	'\./Smileys/[A-Za-z0-9]+/index\.php',
 	'\./Smileys/index\.php',
 	'\./Sources/index\.php',
-	'\./Sources/Actions/index\.php',
-	'\./Sources/Actions/Admin/index\.php',
-	'\./Sources/Actions/Moderation/index\.php',
-	'\./Sources/Actions/Profile/index\.php',
-	'\./Sources/Cache/index\.php',
-	'\./Sources/Cache/APIs/index\.php',
-	'\./Sources/Db/index\.php',
-	'\./Sources/Db/APIs/index\.php',
-	'\./Sources/Graphics/index\.php',
-	'\./Sources/Graphics/Gif/index\.php',
-	'\./Sources/MailAgent/index\.php',
-	'\./Sources/MailAgent/APIs/index\.php',
-	'\./Sources/PackageManager/index\.php',
-	'\./Sources/PersonalMessage/index\.php',
-	'\./Sources/Search/index\.php',
-	'\./Sources/Search/APIs/index\.php',
-	'\./Sources/Subscriptions/index\.php',
-	'\./Sources/Subscriptions/PayPal/index\.php',
-	'\./Sources/tasks/index\.php',
-	'\./Sources/TOTP/index\.php',
-	'\./Sources/WebFetch/index\.php',
-	'\./Sources/WebFetch/APIs/index\.php',
-	'\./Sources/Unicode/index\.php',
-	'\./Themes/default/css/index\.php',
-	'\./Themes/default/fonts/index\.php',
-	'\./Themes/default/fonts/sound/index\.php',
-	'\./Themes/default/images/[A-Za-z0-9]+/index\.php',
-	'\./Themes/default/images/index\.php',
+	'\./Sources/[\w/]+/index\.php',
 	'\./Themes/default/index\.php',
-	'\./Themes/default/languages/index\.php',
-	'\./Themes/default/scripts/index\.php',
+	'\./Themes/default/[\w/]+/index\.php',
 	'\./Themes/index\.php',
 
 	// Language Files are ignored as they don't use the License format.
@@ -120,17 +92,6 @@ try
 				{
 					// Some files, *cough* ManageServer *cough* have lots of junk before the license, otherwise this could easily be 500.
 					$contents = fread($file, 4000);
-
-					// Is this a index.php? Maybe it is supposed to not have it.
-					if ($fileInfo->getFilename() == 'index.php')
-					{
-						$ignoreContents = '<\?php\s+\/\/ Try to handle it with the upper level index\.php\. \(it should know what to do\.\)[\r]?\nif \(file_exists\(dirname\(dirname\(__FILE__\)\) \. \'\/index\.php\'\)\)[\r]?\n\s+include \(dirname\(dirname\(__FILE__\)\) \. \'\/index\.php\'\);[\r]?\nelse[\r]?\n\s+exit;[\r]?\n\s+\?>';
-
-						$ignoreContents2 = '<\?php\s+\/\/ Try to handle it with the upper level index\.php\. \(it should know what to do\.\)[\r]?\nif \(file_exists\(dirname\(__DIR__\) \. \'\/index\.php\'\)\)[\r]?\n\s+include \(dirname\(__DIR__\) \. \'\/index\.php\'\);[\r]?\nelse[\r]?\n\s+exit;[\r]?\n\s+\?>';
-
-						if (preg_match('~' . $ignoreContents . '~i', $contents) || preg_match('~' . $ignoreContents2 . '~i', $contents) )
-							continue;
-					}
 
 					// How the license file should look, in a regex type format.
 					$match = array(
